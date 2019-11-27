c3d2a337fc3787368a083be3bc877273d4af55d1
Hong Yeong Ju <abc123472000@naver.com>
2019-11-27 20:45:38 +0900

    알람방 클라이언트 수정 완료

--- app/src/main/java/com/hfad/alarmapplicaion/AlarmSettingActivity.java
+++ app/src/main/java/com/hfad/alarmapplicaion/AlarmSettingActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AlarmSettingActivity extends AppCompatActivity implements View.OnClickListener{
+public class AlarmSettingActivity extends AppCompatActivity implements View.OnClickListener , CompoundButton.OnCheckedChangeListener, TimePicker.OnTimeChangedListener {
 
 
     EditText mRoomName;
@@ -23,7 +25,10 @@ public class AlarmSettingActivity extends AppCompatActivity implements View.OnCl
     CheckBox mSunday;
     Button mSettingButton;
 
-    boolean[] days;
+    int hourOfDay = 0;
+    int minute = 0;
+
+    boolean[] days = new boolean[7];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,26 +45,59 @@ public class AlarmSettingActivity extends AppCompatActivity implements View.OnCl
         mSunday = (CheckBox)findViewById(R.id.sunday);
         mSettingButton = (Button)findViewById(R.id.settingbutton);
 
+        mMonday.setOnCheckedChangeListener(this);
+        mTuesday.setOnCheckedChangeListener(this);
+        mWednesday.setOnCheckedChangeListener(this);
+        mThursday.setOnCheckedChangeListener(this);
+        mFriday.setOnCheckedChangeListener(this);
+        mSaturday.setOnCheckedChangeListener(this);
+        mSunday.setOnCheckedChangeListener(this);
 
-        days = new boolean[7];
+        mSettingButton.setOnClickListener(this);
+
+        mTimePicker.setHour(hourOfDay);
+        mTimePicker.setMinute(minute);
+        mTimePicker.setOnTimeChangedListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        if(v.getId() == R.id.settingbutton){
+            Toast.makeText(getApplicationContext(), "hour"  +String.valueOf(hourOfDay) ,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch(buttonView.getId()){
             case R.id.monday:
-                days[0]= true;
+                days[0] = isChecked;
                 break;
             case R.id.tuesday:
-                days[1]= true;
+                days[1] = isChecked;
                 break;
-            case R.id.monday:
-                days[0]= true;
+            case R.id.wednesday:
+                days[2] = isChecked;
                 break;
-            case R.id.monday:
-                days[0]= true;
+            case R.id.thursday:
+                days[3] = isChecked;
                 break;
-
+            case R.id.friday:
+                days[4] = isChecked;
+                break;
+            case R.id.saturday:
+                days[5] = isChecked;
+                break;
+            case R.id.sunday:
+                days[6] = isChecked;
+                break;
         }
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+        this.hourOfDay = hourOfDay;
+        this.minute = minute;
     }
 }
