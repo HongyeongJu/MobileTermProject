5de283f107fcab53ea5af0409bdf664e77a9717b
ODong-Dong <56753904+ODong-Dong@users.noreply.github.com>
2019-12-15 02:11:08 +0900

    알람2차수정

--- app/src/main/java/com/hfad/alarmapplicaion/AlarmService.java
+++ app/src/main/java/com/hfad/alarmapplicaion/AlarmService.java
@@ -17,6 +17,9 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
+import com.hfad.alarmapplicaion.AlarmRingingActivity;
+import com.hfad.alarmapplicaion.R;
+
 
 public class AlarmService extends Service {
 
@@ -25,10 +28,32 @@ public class AlarmService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "default";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("알람시작")
+                    .setContentText("알람음이 재생됩니다.")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+
+                    .build();
+
+            startForeground(1, notification);
+        }
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Foreground 에서 실행되면 Notification 을 보여줘야 됨
+ /*       // Foreground 에서 실행되면 Notification 을 보여줘야 됨
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Oreo(26) 버전 이후 버전부터는 channel 이 필요함
             String channelId =  createNotificationChannel();
@@ -40,7 +65,7 @@ public class AlarmService extends Service {
                     .build();
 
             startForeground(1, notification);
-        }
+        }*/
 
         // 알람창 호출
         Intent intent1 = new Intent(this, AlarmRingingActivity.class);
@@ -52,15 +77,18 @@ public class AlarmService extends Service {
 
         Log.d("AlarmService", "Alarm");
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             stopForeground(true);
         }
 
-        stopSelf();
+        stopSelf();*/
         Log.d("ODH","Service");
         return START_NOT_STICKY;
     }
-
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
     @RequiresApi(Build.VERSION_CODES.O)
     private String createNotificationChannel() {
         String channelId = "Alarm";
