ef97c434dd6a285d3830a67aa0802fbc515c2d03
Hong Yeong Ju <abc123472000@naver.com>
2019-12-19 06:57:32 +0900

    최종본

--- app/src/main/java/com/hfad/alarmapplicaion/service/SensorService.java
+++ app/src/main/java/com/hfad/alarmapplicaion/service/SensorService.java
@@ -98,7 +98,7 @@ public class SensorService extends Service implements SensorEventListener {
 
         if (sensorManager != null)
             sensorManager.unregisterListener(this);
-        Toast.makeText(getApplicationContext(), "종료"+cnt+"흔들었습니다.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "상태창을 바꿨습니다.", Toast.LENGTH_SHORT).show();
         Log.d("ODH","Sensor Finish");
         try{
             countDownTimer.cancel();
